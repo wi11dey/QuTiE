382033a288d2e561b5dddbee7b4ea80476020fbc
Will Dey <will_dey@college.harvard.edu>
2022-12-05 14:12:39 -0500

    Updates

--- QuTiE.jl
+++ QuTiE.jl
@@ -2,14 +2,39 @@
 
 module QuTiE
 
+import Base: -, ^, ==
 import SciMLOperators: AbstractSciMLOperator as Operator, AbstractSciMLScalarOperator as ScalarOperator, AbstractSciMLLinearOperator as LinearOperator
 using OrderedCollections
 
-export Time, Space, PeriodicSpace, ℤ, ℝ, ∞
+export Time, Space, ℤ, ℝ, ℂ, ∞
 
 const ℤ = Int
 const ℝ = Float64
-const ∞ = Inf
+const ℂ = ComplexF64
+
+const ∞ = Val(Inf)
+(-)(::Val{ Inf}) = Val(-Inf)
+(-)(::Val{-Inf}) = Val( Inf)
+Base.isinf(::Val{ Inf}) = true
+Base.isinf(::Val{-Inf}) = true
+Base.isfinite(::Val{ Inf}) = false
+Base.isfinite(::Val{-Inf}) = false
+Base.isnan(::Val{ Inf}) = false
+Base.isnan(::Val{-Inf}) = false
+(==)(::Val{Inf}, x::AbstractFloat) = Inf == x
+(==)(x::AbstractFloat, ::Val{Inf}) = x == Inf
+(==)(::Val{-Inf}, x::AbstractFloat) = -Inf == x
+(==)(x::AbstractFloat, ::Val{-Inf}) = x == -Inf
+Base.isless(::Val{Inf}, x) = isless(Inf, x)
+Base.isless(x, ::Val{Inf}) = isless(x, Inf)
+Base.isless(::Val{Inf}, ::Val{Inf}) = false
+Base.isless(::Val{-Inf}, x) = isless(-Inf, x)
+Base.isless(x, ::Val{-Inf}) = isless(x, -Inf)
+Base.isless(::Val{-Inf}, ::Val{-Inf}) = false
+
+const Compactification{T} = Union{T, Val{-Inf}, Val{Inf}}
+Base.typemin(::Type{>: Val{-Inf}}) = -∞
+Base.typemax(::Type{>: Val{ Inf}}) =  ∞
 
 (^)(x::Operator, n::Int) = prod(Iterators.repeated(x, n))
 
@@ -17,16 +42,45 @@ abstract type Coordinate{T <: Real} <: Operator{T} end
 
 struct Time <: Coordinate{ℝ} end
 
-mutable struct Space{T, Periodic} <: Coordinate{T}
-    const lower::T
-    const upper::T
+Base.eps(x::Integer) = oneunit(x)
+
+mutable struct Space{T} <: Coordinate{T}
+    const lower::Compactification{T}
+    const upper::Compactification{T}
+    const periodic::Bool
 
     indices::AbstractRange{T}
-end
-(::Type{Space{T}})(args...) where T = Space{T, false}(args...)
-(space::Type{<: Space})(upper) = space(zero(upper), upper)
 
-const PeriodicSpace{T} = Space{T, true}
+    function Space{T}(lower::Compactification{T},
+                      upper::Compactification{T};
+                      periodic=false,
+                      step::Union{T, Nothing}=nothing) where T
+        if (isinf(lower) || isinf(upper)) && periodic
+            throw(ArgumentError("Unbounded space cannot be periodic"))
+        end
+        if lower == upper
+            throw(ArgumentError("Null space"))
+        end
+        new(min(lower, upper), max(lower, upper), periodic, step === 1 ? lower:upper : lower:step:upper)
+    end
+end
+Space(upper) = Space(zero(upper), upper)
+Space(lower, step, upper; keywords...) = Space(lower, upper; step=step, keywords...)
+Space{Bool}() = Space{Bool}(0, 1)
+
+struct ProductSpace{N, T} <: Operator{NTuple{N, T}}
+    factors::NTuple{N, Space{T}}
+end
+ProductSpace{N, T}() where {N, T} = ProductSpace{N, T}(ntuple(_ -> Space{T}(), Val{N}))
+ProductSpace(factors::Space...) = ProductSpace(factors)
+ProductSpace(product::ProductSpace, additional::Space...) = ProductSpace((product.factors..., additional...))
+
+Base.getindex(product::ProductSpace, n) = product.factors[n]
+
+const × = ProductSpace
+
+const Qubit = Space{Bool}
+const Qubits{N} = ProductSpace{N, Bool}
 
 struct Derivative{N} end
 (::Type{Derivative})(args...) = Derivative{1}(args...)
@@ -34,8 +88,8 @@ struct Derivative{N} end
 const ∂  = Derivative{1}
 const ∂² = ∂^2
 const ∂³ = ∂^3
-(::Type{Derivative{1}})(wrt::Space{T, Periodic}) where {T, Periodic} = FunctionOperator(isinplace=true, T) do (dψ, ψ, p, t)
-    dψ .= (diff([Periodic ? ψ[end] : zero(T); ψ]) + diff([a; Periodic ? ψ[begin] : zero(T)]))/2
+(::Type{Derivative{1}})(wrt::Space{T <: AbstractFloat}) where T = FunctionOperator(isinplace=true, T) do (dψ, ψ, p, t)
+    dψ .= (diff([Periodic ? ψ[end] : zero(T); ψ]) + diff([a; wrt.periodic ? ψ[begin] : zero(T)]))/2
 end
 (::Type{Derivative{N}})(wrt::Space) where N = ∂(wrt)^N
 
@@ -48,16 +102,22 @@ function Base.axes(op::Operator)
     (dims...,)
 end
 
-struct State{N} <: AbstractArray{ComplexF64, N}
+struct State{N} <: AbstractArray{ℂ, N}
     ax::NTuple{N, Space}
-    data::Vector{ComplexF64}
+    data::Vector{ℂ}
 end
+Base.axes(ψ::State) = ψ.ax
+
+function ⊗(ψ::State{N}, φ::State{M}) where {N, M}
+    ax = (axes(ψ)..., axes(φ)...)
+    State(ax, [ψ[i[begin:N]...]*φ[i[N + 1:end]...] for i in CartesianIndices(ax)])
+end
+⊗(ψ::State{N}, φ::State{M}, rest::State...) = ⊗(ψ ⊗ φ, rest...)
 
 Base.getindex(ψ::State{N}, inds::NTuple{N, Int}...) where N = ψ.data[LinearIndices(ax)[inds...]]
 
 Base.similar(::Type{State}, ax::NTuple{N, Space}) where N = similar(State{N}, ax)
-function Base.similar(::Type{State{N}}, ax::NTuple{N, Space}) where N
-end
+Base.similar(::Type{State{N}}, ax::NTuple{N, Space}) where N = State(ax, Vector{ℂ}(undef, length(CartesianIndices(ax))))
 
 Base.fill(value::ComplexF64, ax::Tuple{Vararg{Space}}) = fill!(similar(State, ax), value)
 Base.zeros(T::Type{ComplexF64}, ax::Tuple{Vararg{Space}}) = fill(zero(ComplexF64), ax)
@@ -71,6 +131,9 @@ using ..QuTiE
 using Revise
 using PhysicalConstants.CODATA2018
 using DifferentialEquations
+
+export ħ²
+ħ² = ħ^2
 
 __revise_mode__ = :evalassign
 
--- example.jl
+++ example.jl
@@ -8,7 +8,7 @@ V(x, t) = -k*x
 ∇ = ∂(x)
 H = -ħ^2/(2m)*∇^2 + V(x, t)
 # Alternatively:
-H = -ħ^2/(2m)*∂^2(x) + V(x, t)
+H = -ħ²/(2m)*∂²(x) + V(x, t)
 
 # Initial Gaussian probability distribution for particle:
 ψ₀=ℯ^(-x^2)
