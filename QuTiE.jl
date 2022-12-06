219183df4f25c9e15ca9c5d638f613b0d39eb709
Will Dey <will_dey@college.harvard.edu>
2022-12-05 21:49:48 -0500

    Condensation

--- QuTiE.jl
+++ QuTiE.jl
@@ -3,8 +3,8 @@
 module QuTiE
 
 import Base: -, ^, ==
-import SciMLOperators: AbstractSciMLOperator as Operator, AbstractSciMLScalarOperator as ScalarOperator, AbstractSciMLLinearOperator as LinearOperator
-using OrderedCollections
+import SciMLOperators: AbstractSciMLOperator as Operator, AbstractSciMLScalarOperator as ScalarOperator, AbstractSciMLLinearOperator as LinearOperator, getops
+import AbstractTrees
 
 export Time, Space, ℤ, ℝ, ℂ, ∞, Qubit, Qubits
 
@@ -21,10 +21,10 @@ Base.isfinite(::Val{ Inf}) = false
 Base.isfinite(::Val{-Inf}) = false
 Base.isnan(::Val{ Inf}) = false
 Base.isnan(::Val{-Inf}) = false
-(==)(::Val{Inf}, x::AbstractFloat) = Inf == x
-(==)(x::AbstractFloat, ::Val{Inf}) = x == Inf
-(==)(::Val{-Inf}, x::AbstractFloat) = -Inf == x
-(==)(x::AbstractFloat, ::Val{-Inf}) = x == -Inf
+==(::Val{Inf}, x::AbstractFloat) = Inf == x
+==(x::AbstractFloat, ::Val{Inf}) = x == Inf
+==(::Val{-Inf}, x::AbstractFloat) = -Inf == x
+==(x::AbstractFloat, ::Val{-Inf}) = x == -Inf
 Base.isless(::Val{Inf}, x) = isless(Inf, x)
 Base.isless(x, ::Val{Inf}) = isless(x, Inf)
 Base.isless(::Val{Inf}, ::Val{Inf}) = false
@@ -39,9 +39,14 @@ const Compactification{T <: Number} = Union{T, Val{-Inf}, Val{Inf}}
 Base.typemin(::Type{>: Val{-Inf}}) = -∞
 Base.typemax(::Type{>: Val{ Inf}}) =  ∞
 
-(^)(x::Operator, n::Int) = prod(Iterators.repeated(x, n))
+(^)(op::Operator, n::Int) = prod(Iterators.repeated(op, n))
+AbstractTrees.children(op::Operator) = getops(op)
 
 abstract type Coordinate{T <: Real} <: Operator{T} end
+depends(op::Operator, x::Coordinate) = x ∈ AbstractTrees.Leaves(op)
+
+getops(::Coordinate) = ()
+AbstractTrees.children(::Coordinate) = ()
 
 struct Time <: Coordinate{ℝ} end
 
@@ -50,42 +55,42 @@ mutable struct Space{T} <: Coordinate{T}
     const upper::Compactification{T}
     const periodic::Bool
 
+    # v2: resample/interpolate when grid too large or not enough samples, along with minimum grid spacing, also optional samples option
+    α::Union{T, Nothing}
     ε::real(ℂ)
-    α::T
     samples::UnitRange{Int}
 
     indices::AbstractRange{T}
 
-    # v2: resample/interpolate when grid too large or not enough samples, along with minimum grid spacing, also optional samples option
     function Space{T}(lower,
                       upper;
                       periodic=false,
-                      ε=1e-5, # Minimum complex modulus
-                      α=nothing,
                       first=nothing,
-                      step::Union{T, Nothing}=nothing,
+                      step=nothing,
                       last=nothing,
-                      samples::UnitRange{Int}=1:typemax(Int)) where T
-        if (isinf(lower) || isinf(upper)) && periodic
-            throw(ArgumentError("Unbounded space cannot be periodic"))
-        end
-        if lower == upper
-            throw(ArgumentError("Null space"))
-        end
+                      α=nothing, # Scale factor (maximum grid spacing).
+                      ε=1e-5, # Minimum complex modulus.
+                      samples=1:typemax(Int) #= Minimum and maximum number of samples. =#) where T
+        (isinf(lower) || isinf(upper)) && periodic && throw(ArgumentError("Unbounded space cannot be periodic"))
+        lower == upper && throw(ArgumentError("Null space"))
+
         lower, upper = min(lower, upper), max(lower, upper)
-        if isnothing(first)
-            first = isfinite(lower) ? lower : -5
-        end
-        if isnothing(last)
-            last = isfinite(upper) ? upper : 5
-        end
-        if isnothing(step)
-            step = T <: Integer ? one(T) : (upper - lower)/100
-        end
-        if isnothing(α)
-            α = step
-        end
-        new(lower, upper, periodic, ε, α, isone(step) ? first:last : first:step:last)
+
+        isnothing(first) && first = isfinite(lower) ? lower : -5
+        isnothing(last)  && last  = isfinite(upper) ? upper :  5
+        isnothing(step) && step = T <: Integer ? one(T) : (upper - lower)/100
+
+        new(
+            lower,
+            upper,
+            periodic,
+
+            α,
+            ε,
+            samples,
+
+            isone(step) ? first:last : first:step:last
+        )
     end
 end
 Space(upper) = Space(zero(upper), upper)
@@ -94,19 +99,8 @@ Space(lower, upper, range::AbstractRange; keywords...) = Space(lower, upper; fir
 Space(range::AbstractRange{T}; keywords...) where T = Space{T}(first(range), last(range); step=step(range), keywords...)
 Space{Bool}() = Space{Bool}(0, 1)
 
-struct ProductSpace{N, T} <: Operator{NTuple{N, T}}
-    factors::NTuple{N, Space{T}}
-end
-ProductSpace{N, T}() where {N, T} = ProductSpace{N, T}(ntuple(_ -> Space{T}(), Val{N}))
-ProductSpace(factors::Space...) = ProductSpace(factors)
-ProductSpace(product::ProductSpace, additional::Space...) = ProductSpace((product.factors..., additional...))
-
-Base.getindex(product::ProductSpace, n) = product.factors[n]
-
-const × = ProductSpace
-
-const Qubit = Space{Bool}
-const Qubits{N} = ProductSpace{N, Bool}
+==(a::Space, b::Space) = a === b
+Base.hash(a::Space) = objectid(a)
 
 struct Derivative{N} end
 (::Type{Derivative})(args...) = Derivative{1}(args...)
@@ -119,21 +113,36 @@ const ∂³ = ∂^3
 end
 (::Type{Derivative{N}})(wrt::Space) where N = ∂(wrt)^N
 
-function Base.axes(op::Operator)
-    dims = OrderedSet{Space}()
-    walk(x::Space) = push!(x, dims)
-    walk(op::Operator) = walk.(getops(op))
-    walk(x) = nothing
-    walk.(getops(op))
-    (dims...,)
+struct ProductSpace{N, T} <: Operator{NTuple{N, T}}
+    factors::NTuple{N, Space{T}}
 end
+ProductSpace{N, T}() where {N, T} = ProductSpace{N, T}(ntuple(_ -> Space{T}(), Val{N}))
+ProductSpace(factors::Space...) = ProductSpace(factors)
+ProductSpace(factors::Union{Space, ProductSpace}...) = ProductSpace(AbstractTrees.Leaves(factors)...)
+
+getops(::Coordinate) = ()
+AbstractTrees.children(product::ProductSpace) = product.factors
+AbstractTrees.childtype(::Type{ProductSpace{N, T}}) where {N, T} = Space{T}
+AbstractTrees.childtype(::ProductSpace{N, T}) where {N, T} = AbstractTrees.childtype(ProductSpace{N, T})
+Base.getindex(product::ProductSpace, n) = product.factors[n]
+
+const × = ProductSpace
+
+const Qubit = Space{Bool}
+const Qubits{N} = ProductSpace{N, Bool}
+
+const HigherDimensionalSpace{N} = NTuple{N, Space}
+Base.axes(op::Operator) = filter(x -> x isa Space, AbstractTrees.Leaves(op)) |> unique! |> Tuple
 
 struct State{N} <: AbstractArray{ℂ, N}
-    ax::NTuple{N, Space}
+    ax::HigherDimensionalSpace{N}
     data::Vector{ℂ}
 end
 Base.axes(ψ::State) = ψ.ax
 
+"""
+Tensor product of two states.
+"""
 function ⊗(ψ::State{N}, φ::State{M}) where {N, M}
     ax = (axes(ψ)..., axes(φ)...)
     State(ax, [ψ[i[begin:N]...]*φ[i[N + 1:end]...] for i in CartesianIndices(ax)])
@@ -142,8 +151,8 @@ end
 
 Base.getindex(ψ::State{N}, inds::NTuple{N, Int}...) where N = ψ.data[LinearIndices(ax)[inds...]]
 
-Base.similar(::Type{State}, ax::NTuple{N, Space}) where N = similar(State{N}, ax)
-Base.similar(::Type{State{N}}, ax::NTuple{N, Space}) where N = State(ax, Vector{ℂ}(undef, length(CartesianIndices(ax))))
+Base.similar(::Type{State}, ax::HigherDimensionalSpace{N}) where N = similar(State{N}, ax)
+Base.similar(::Type{State{N}}, ax::HigherDimensionalSpace{N}) where N = State(ax, Vector{ℂ}(undef, length(CartesianIndices(ax))))
 
 Base.fill(value::ComplexF64, ax::Tuple{Vararg{Space}}) = fill!(similar(State, ax), value)
 Base.zeros(T::Type{ComplexF64}, ax::Tuple{Vararg{Space}}) = fill(zero(ComplexF64), ax)
