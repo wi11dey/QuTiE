c910058bb4edc67d625a21c2a7001838f1ede4aa
Will Dey <will_dey@college.harvard.edu>
2022-12-14 12:48:32 -0500

    Guix environment

---
+++ QuTiE.scm
@@ -0,0 +1,33 @@
+(use-modules (guix utils)
+	     (guix packages)
+	     (gnu packages julia)
+	     (gnu packages julia-jll))
+
+(packages->manifest
+ (list (package
+	(inherit julia)
+	(arguments
+	 (substitute-keyword-arguments
+	  (package-arguments julia)
+	  ((#:phases phases)
+	   `(modify-phases
+	     ,phases
+	     ;; Additional packages expected but not correctly symlinked by installation nor `symlink-missing-libraries':
+	     (add-after 'symlink-missing-libraries 'symlink-missing-libraries-2
+			(lambda* (#:key inputs outputs #:allow-other-keys)
+			  (let* ((out (assoc-ref outputs "out"))
+				 (link
+				  (lambda (pkgname pred)
+				    (map (lambda (file)
+					   (unless (file-exists?
+						    (string-append out "/lib/julia/"
+								   (basename file)))
+					     (symlink file (string-append out "/lib/julia/"
+									  (basename file)))))
+					 (find-files (string-append (assoc-ref inputs pkgname)
+								    "/lib") pred)))))
+			    (link "suitesparse" "libklu\\.so"))))))
+	  ((#:tests? _) #f))))
+       julia-sundials-jll
+       julia-glfw-jll
+       julia-cairo-jll))
