caf4dd865cb75f189b299c8a8ae69f80ec6d6059
Will Dey <will_dey@college.harvard.edu>
2023-03-12 21:52:14 -0400

    Vacuum state

--- src/state.jl
+++ src/state.jl
@@ -2,7 +2,7 @@ struct State{N, D <: Volume{N}, Orig <: AbstractArray{ℂ, N}, Interp <: Abstrac
     data::DimArray{        ℂ, N, D, Orig}
     interpolated::DimArray{ℂ, N, D, Interpolation{ℂ, N, Interp}}
 
-    function DimensionalData.rebuild(ψ::State, data::AbstractArray, dims::Volume, refdims::Tuple, name, metdata)
+    function DimensionalData.rebuild(::Union{State, Vacuum}, data::AbstractArray, dims::Volume, refdims::Tuple, name, metdata)
         @boundscheck length(unique(dims)) == length(dims) || throw(DimensionMismatch("Duplicate dimensions"))
         sz = length.(dims)
         @boundscheck length(data) == sz || throw(DimensionMismatch("Mismatch between product of dimensions and length of data"))
@@ -32,38 +32,8 @@ struct State{N, D <: Volume{N}, Orig <: AbstractArray{ℂ, N}, Interp <: Abstrac
             DimArray(itp, set.(dims, DimensionalData.NoLookup()))
         )
     end
-
-    @propagate_inbounds function State{N, D}(dims::D, data::Vector{ℂ}) where {N, D}
-        @boundscheck length(unique(dims)) == length(dims) || throw(DimensionMismatch("Duplicate dimensions"))
-        sz = length.(dims)
-        @boundscheck length(data) == sz || throw(DimensionMismatch("Mismatch between product of dimensions and length of data"))
-        reshaped = Base.ReshapedArray(data, sz, ())
-        spaces = Space.(dims)
-        spec = map(spaces) do space
-            isfield(space) || return NoInterp()
-            BSpline(Quadratic(ifelse(isperiodic(space), Periodic, Natural)(OnCell())))
-        end
-        padded = Interpolations.padded_axes(dims, spec)
-        itp = extrapolate(extrapolate(
-            scale(Interpolations.BSplineInterpolation(
-                ℂ,
-                OffsetArray(Base.ReshapedArray(Vector{ℂ}(
-                    undef,
-                    padded
-                    .|> length
-                    |> prod
-                ), sz, ()), padded),
-                spec,
-                dims
-            ), dims...),
-            ifelse.(isperiodic.(spaces), Periodic(), Throw())
-        ), zero(ℂ)) |> Interpolation
-        new{N, D, typeof(reshaped), typeof(itp)}(
-            DimArray(reshaped, dims),
-            DimArray(itp, set.(dims, DimensionalData.NoLookup()))
-        )
-    end
 end
+@propagate_inbounds State{N, D}(dims::D, data::Vector{ℂ}) where {N, D} = rebuild(Vacuum(), data)
 State{N}(dims::Volume{N}) where N = @inbounds State{N}(dims, Vector{ℂ}(undef, dims .|> length |> prod))
 function State{N}(ψ₀::Operator) where N
 end
@@ -76,3 +46,9 @@ end
 
 struct Interpolated <: DimensionalData.ArraySelector
 end
+
+@generated Base.getindex(ψ::State, indices...) =
+    # Multiplexes 2 child DimArrays:
+    (any(indices.parameters .<: Interpolated)
+     ? :(ψ.interpolated[indices...])
+     : :(ψ.data[        indices...]))
