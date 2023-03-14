186f3ed85a04f2dd2d67f371bb90b4e28103ba25
Will Dey <will_dey@college.harvard.edu>
2023-03-14 11:11:33 -0400

    Need new commutator algorithm later

--- src/commute.jl
+++ src/commute.jl
@@ -1,9 +1,4 @@
 export commutator, anticommutator
 
-function commutator(a::Operator, b::Operator)
-    isdisjoint(    getindex.(∂, filter_type(Dimension, a)), filter_type(∂, b)) &&
-        isdisjoint(getindex.(∂, filter_type(Dimension, b)), filter_type(∂, a)) &&
-        return false # a and b commute.
-    a*b - b*a
-end
+commutator(    a::Operator, b::Operator) = a*b - b*a
 anticommutator(a::Operator, b::Operator) = a*b + b*a
