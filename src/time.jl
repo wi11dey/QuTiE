dbac59d5db7138bccc25c8183bf9b4df757aba2a
Will Dey <will_dey@college.harvard.edu>
2023-03-13 20:43:09 -0400

    Applying time

--- src/time.jl
+++ src/time.jl
@@ -11,3 +11,6 @@ isclassical(::Time) = true  # N + 1 dimensional formulation.
 isperiodic( ::Time) = false # No time travel.
 isconstant( ::Time) = false # By construction.
 Base.show(io::IO, ::Time) = print(io, "t")
+
+(*)(                   τ::Time, _) = τ.t
+LinearAlgebra.mul!(du, τ::Time, u) = du .= τ*u
